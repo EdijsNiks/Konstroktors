0411f507399285bcadba3477f191d822f015a17e
ene <edijsniks@gmail.com>
2022-11-23 18:28:25 +0200

    "Added the config read"

--- naked.py
+++ naked.py
@@ -3,16 +3,27 @@ import json
 import datetime
 import time
 import yaml
+from configparser import ConfigParser
+
+
+
 
 from datetime import datetime
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
+#Reading API
+try:
+                config = ConfigParser()
+                config.read('config.ini')
 
-#Giving nasa API to read 
-nasa_api_key = "09tpWUgGW7TWzUusM3DJ8CT0NHeLddyzc28cyZlc"
-nasa_api_url = "https://api.nasa.gov/neo/"
+                nasa_api_key = config.get('nasa', 'api_key')
+                nasa_api_url = config.get('nasa', 'api_url')
+              
+except:
+                logger.exception('')
+print('DONE')
 
 # Getting todays date
 dt = datetime.now()
